63dbd9393a9932c6f3cffed07c1051e8fd98c6d6
Ryuta <ryuta_26ers@icloud.com>
2019-08-11 15:35:56 +0900

    Initial Commit

---
+++ 目的貯金/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  目的貯金
+//
+//  Created by  RyutaNishioka on 2019/08/11.
+//  Copyright © 2019 Ryuta Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
